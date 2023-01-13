990a637e11de238188478a97ae9db40dfb58ecec
it20228644 <it20228644@my.sliit.lk>
2023-01-13 12:00:12 +0530

    interface and moves updated till game over

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,19 +31,20 @@ namespace Flappy_Ball
         {
             this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.life1 = new System.Windows.Forms.PictureBox();
+            this.life2 = new System.Windows.Forms.PictureBox();
+            this.life3 = new System.Windows.Forms.PictureBox();
             this.lblScore = new System.Windows.Forms.Label();
             this.gameTime = new System.Windows.Forms.Timer(this.components);
-            this.pictureBox7 = new System.Windows.Forms.PictureBox();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.downWall = new System.Windows.Forms.PictureBox();
             this.ground = new System.Windows.Forms.PictureBox();
             this.upWall = new System.Windows.Forms.PictureBox();
             this.ball = new System.Windows.Forms.PictureBox();
+            this.lblGameOver = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.life1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.life2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.life3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.downWall)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.upWall)).BeginInit();
@@ -53,22 +54,51 @@ namespace Flappy_Ball
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.AntiqueWhite;
-            this.panel1.Controls.Add(this.pictureBox7);
-            this.panel1.Controls.Add(this.pictureBox6);
-            this.panel1.Controls.Add(this.pictureBox5);
+            this.panel1.Controls.Add(this.life1);
+            this.panel1.Controls.Add(this.life2);
+            this.panel1.Controls.Add(this.life3);
             this.panel1.Location = new System.Drawing.Point(13, 398);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(135, 39);
             this.panel1.TabIndex = 4;
             // 
+            // life1
+            // 
+            this.life1.Image = global::Flappy_Ball.Properties.Resources.life;
+            this.life1.Location = new System.Drawing.Point(88, -1);
+            this.life1.Name = "life1";
+            this.life1.Size = new System.Drawing.Size(40, 40);
+            this.life1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.life1.TabIndex = 0;
+            this.life1.TabStop = false;
+            // 
+            // life2
+            // 
+            this.life2.Image = global::Flappy_Ball.Properties.Resources.life;
+            this.life2.Location = new System.Drawing.Point(48, -1);
+            this.life2.Name = "life2";
+            this.life2.Size = new System.Drawing.Size(40, 40);
+            this.life2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.life2.TabIndex = 0;
+            this.life2.TabStop = false;
+            // 
+            // life3
+            // 
+            this.life3.Image = global::Flappy_Ball.Properties.Resources.life;
+            this.life3.Location = new System.Drawing.Point(8, -1);
+            this.life3.Name = "life3";
+            this.life3.Size = new System.Drawing.Size(40, 40);
+            this.life3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.life3.TabIndex = 0;
+            this.life3.TabStop = false;
+            // 
             // lblScore
             // 
-            this.lblScore.Font = new System.Drawing.Font("OCR A Extended", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Font = new System.Drawing.Font("OCR A Extended", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblScore.Location = new System.Drawing.Point(450, 398);
             this.lblScore.Name = "lblScore";
             this.lblScore.Size = new System.Drawing.Size(100, 39);
             this.lblScore.TabIndex = 5;
-            this.lblScore.Text = "Score";
             this.lblScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // gameTime
@@ -77,40 +107,10 @@ namespace Flappy_Ball
             this.gameTime.Interval = 20;
             this.gameTime.Tick += new System.EventHandler(this.gameTimeTickEvent);
             // 
-            // pictureBox7
-            // 
-            this.pictureBox7.Image = global::Flappy_Ball.Properties.Resources.life;
-            this.pictureBox7.Location = new System.Drawing.Point(88, -1);
-            this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(40, 40);
-            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox7.TabIndex = 0;
-            this.pictureBox7.TabStop = false;
-            // 
-            // pictureBox6
-            // 
-            this.pictureBox6.Image = global::Flappy_Ball.Properties.Resources.life;
-            this.pictureBox6.Location = new System.Drawing.Point(48, -1);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(40, 40);
-            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox6.TabIndex = 0;
-            this.pictureBox6.TabStop = false;
-            // 
-            // pictureBox5
-            // 
-            this.pictureBox5.Image = global::Flappy_Ball.Properties.Resources.life;
-            this.pictureBox5.Location = new System.Drawing.Point(8, -1);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(40, 40);
-            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox5.TabIndex = 0;
-            this.pictureBox5.TabStop = false;
-            // 
             // downWall
             // 
             this.downWall.Image = global::Flappy_Ball.Properties.Resources.up_wall;
-            this.downWall.Location = new System.Drawing.Point(406, 225);
+            this.downWall.Location = new System.Drawing.Point(360, 230);
             this.downWall.Name = "downWall";
             this.downWall.Size = new System.Drawing.Size(53, 159);
             this.downWall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -147,24 +147,39 @@ namespace Flappy_Ball
             this.ball.TabIndex = 0;
             this.ball.TabStop = false;
             // 
+            // lblGameOver
+            // 
+            this.lblGameOver.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblGameOver.Font = new System.Drawing.Font("OCR A Extended", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameOver.ForeColor = System.Drawing.Color.Navy;
+            this.lblGameOver.Location = new System.Drawing.Point(96, 118);
+            this.lblGameOver.Name = "lblGameOver";
+            this.lblGameOver.Size = new System.Drawing.Size(383, 133);
+            this.lblGameOver.TabIndex = 6;
+            this.lblGameOver.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(562, 450);
+            this.Controls.Add(this.upWall);
+            this.Controls.Add(this.downWall);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.lblGameOver);
             this.Controls.Add(this.lblScore);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.downWall);
             this.Controls.Add(this.ground);
-            this.Controls.Add(this.upWall);
-            this.Controls.Add(this.ball);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.Name = "Form1";
             this.Text = "Flappy Ball";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyDownEvent);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyUpEvent);
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.life1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.life2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.life3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.downWall)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.upWall)).EndInit();
@@ -180,11 +195,12 @@ namespace Flappy_Ball
         private System.Windows.Forms.PictureBox ground;
         private System.Windows.Forms.PictureBox downWall;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox7;
-        private System.Windows.Forms.PictureBox pictureBox6;
-        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox life1;
+        private System.Windows.Forms.PictureBox life2;
+        private System.Windows.Forms.PictureBox life3;
         private System.Windows.Forms.Label lblScore;
         private System.Windows.Forms.Timer gameTime;
+        private System.Windows.Forms.Label lblGameOver;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,16 +14,116 @@ namespace Flappy_Ball
         int wallSpeed = 5;
         int gameScore = 0;
         int lifeCount = 3;
+        int lifeFreeTime = 0;
         public Form1()
-        {
+        { 
             InitializeComponent();
         }
 
         private void gameTimeTickEvent(object sender, EventArgs e)
         {
+
+            ballFadingEffect();
+
+            lblGameOver.Visible = false;
+
+            lifeFreeTime += 20;
+
             ball.Top += gravity;
             downWall.Left -= wallSpeed;
             upWall.Left -= wallSpeed;
+
+            if (upWall.Left < -150)
+            {
+                upWall.Left = 450;
+                gameScore += 10;
+            }
+            if (downWall.Left < -80)
+            {
+                downWall.Left = 580;
+                gameScore += 10;
+            }
+
+            if (ball.Bounds.IntersectsWith(upWall.Bounds) || ball.Bounds.IntersectsWith(downWall.Bounds) || ball.Bounds.IntersectsWith(ground.Bounds))
+            {
+                endGame();
+            }
+
+            lblScore.Text = "" + gameScore;
+        }
+
+        private void keyUpEvent(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 8;
+            }
+        }
+
+        private void keyDownEvent(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space)
+            {
+                gravity = -8;
+            }
+        }
+
+        private void endGame()
+        {
+            if(lifeCount == 0 && lifeFreeTime > 3000)
+            {
+                gameTime.Stop();
+                lblGameOver.Text = "Game Over! \n Your Score is : " + gameScore;
+                lblGameOver.Visible = true;
+            }
+            else
+            {
+                switch (lifeCount)
+                {
+                    case 3:
+                        if(lifeFreeTime > 2500)
+                        {
+                            life1.Visible = false;
+                            lifeCount--;
+                            lifeFreeTime = 0;
+                        }
+                        break;
+                    case 2:
+                        if(lifeFreeTime > 2500)
+                        {
+                            life2.Visible = false;
+                            lifeCount--;
+                            lifeFreeTime = 0;
+                        }
+                        break;
+                    case 1: 
+                        if(lifeFreeTime > 2500)
+                        {
+                            life3.Visible = false;
+                            lifeCount--;
+                            lifeFreeTime = 0;
+                        }
+                        break;
+                }
+                    
+
+            }
+        }
+
+        private void ballFadingEffect()
+        {
+            if(ball.Visible == true && lifeFreeTime < 2500)
+            {
+                ball.Visible = false;
+            }
+            else if (ball.Visible == false && lifeFreeTime < 2500)
+            {
+                ball.Visible = true;
+            }
+            else
+            {
+                ball.Visible = true;
+            }
         }
     }
 }
